7d48c43a03d96564cf1b7d634cc0e58bf414253a
André Oliveira <andre.a.c.oliveira.outlook.com>
2023-03-18 11:29:44 +0000

    more fixes to nvme RAID

--- NVMe/02_create_raid.sh
+++ NVMe/02_create_raid.sh
@@ -4,4 +4,4 @@ set -Euo pipefail
 
 mdadm --create /dev/md2 --level=1 --raid-disks=2 /dev/nvme0n1p2 /dev/nvme0n2p2
 
-mdadm --create /dev/md2 --level=1 --raid-disks=2 /dev/nvme0n2p3 /dev/nvme0n2p3
+mdadm --create /dev/md3 --level=1 --raid-disks=2 /dev/nvme0n2p3 /dev/nvme0n2p3
